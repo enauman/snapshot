47fc7c3a82d4399d30e356b1a39e3fbea92a6441
enauman <enauman@users.noreply.github.com>
2019-03-30 10:40:14 -0400

    Update snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -5,10 +5,14 @@ import argparse
 parser = argparse.ArgumentParser(description="one shot script")
 parser.add_argument('-hf','--hflip', help='flip horizontally if True, default False',required=False)
 parser.add_argument('-vf','--vflip',help='flip vertically if True, default False', required=False)
+parser.add_argument('-I','--iso',help='set ISO, default 200',required=False)
 args = parser.parse_args()
 camera = PiCamera(resolution=(1280, 720), framerate=30)
 # Set ISO to the desired value
-camera.iso = 200
+if(not args.iso):
+        camera.iso = 200
+else:
+        camera.iso = int(args.iso)
 # Wait for the automatic gain control to settle
 sleep(2)
 # Now fix the values
@@ -18,11 +22,11 @@ g = camera.awb_gains
 camera.awb_mode = 'off'
 camera.awb_gains = g
 camera.hflip = args.hflip
-print('hflip = ',camera.hflip)
 camera.vflip = args.vflip
-print('vflip = ',camera.vflip)
 camera.start_preview()
 # Camera warm-up time
 sleep(2)
-camera.capture('snap.jpg')
+camera.capture('snapshot.jpg')
+print("saved your 'snapshot.jpg' file.")
 camera.close()
+
